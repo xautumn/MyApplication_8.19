ea2e94d605efd6a8b72669752006877f5fcabb4c
xautumn <1635916212>
2016-09-02 22:29:49 +0800

    test

--- app/src/main/java/com/example/administrator/myapplication_819/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication_819/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.administrator.myapplication_819;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         View viewById = findViewById(R.id.tv_test_title);
+
+    }
+    public  void  test3(){
+
+
+        Log.i("11","大油桃");
+
 
     }
 }
